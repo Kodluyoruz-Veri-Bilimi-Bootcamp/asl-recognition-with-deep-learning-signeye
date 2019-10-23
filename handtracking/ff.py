d9c0534b72157b73dc3b4b19d77c5dd124b23d91
nbsas <asimnecip_arslan@hotmail.com>
2019-10-23 22:16:44 +0300

    try4 handtracking folder updated

---
+++ handtracking/ff.py
@@ -0,0 +1,14 @@
+def nextSquare(): 
+    i = 1; 
+  
+    # An Infinite loop to generate squares  
+    while True: 
+        yield i*i                 
+        i += 1  # Next execution resumes  
+                # from this point 
+
+
+gen = nextSquare()
+
+for i in gen:
+	print(i)
